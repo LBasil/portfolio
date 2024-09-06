f025a5a77a90ba6c6db6005f97d340a3ea9a7210
LBasil <basil.lhote@gmail.com>
2024-09-06 16:53:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal('.card', {
+    interval: 200,
+    origin: 'bottom',
+    distance: '50px',
+    duration: 800,
+    reset: true
+});
+
+ScrollReveal().reveal('h2, p', {
+    interval: 200,
+    origin: 'top',
+    distance: '50px',
+    duration: 800,
+    reset: true
+});
